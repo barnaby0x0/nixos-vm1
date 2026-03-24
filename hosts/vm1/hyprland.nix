b1727f8686794e2103e4f52af79fb32c3a722b57
Victor <victor@mail.com>
2026-03-24 17:23:28 +0100

    .

--- hosts/vm1/hyprland.nix
+++ hosts/vm1/hyprland.nix
@@ -1,7 +1,16 @@
 {inputs, pkgs, ...}: {
+  # programs.hyprland = {
+  #   enable = true;
+  #   package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+  #   portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  # };
+
+
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    withUWSM = true; # recommended for most users
+    xwayland.enable = true; # Xwayland can be disabled.
   };
+
+
 }
