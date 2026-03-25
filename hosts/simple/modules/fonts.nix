ddc96a1021a444a0d9e86a9806ef991d3178793e
Victor <victor@mail.com>
2026-03-25 18:59:25 +0100

    .

--- hosts/simple/modules/fonts.nix
+++ hosts/simple/modules/fonts.nix
@@ -3,7 +3,6 @@
   fonts.packages = with pkgs; [
     font-awesome_4          # Important : c’est souvent celle utilisée par les configs par défaut de Waybar
     font-awesome            # Version plus récente (6 ou 7)
-    nerdfonts               # Ou mieux, seulement les symbols si tu veux économiser
     # Alternative plus légère pour les symbols seulement :
     # (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
     (nerdfonts.override { fonts = [ "JetBrainsMono" "NerdFontsSymbolsOnly" ]; })
