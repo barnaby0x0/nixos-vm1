4ff414fb6a51b53f88699e88c0ec43b24efbb5ba
Victor <victor@mail.com>
2026-03-25 17:23:17 +0100

    .

--- hosts/simple/modules/hyprland.nix
+++ hosts/simple/modules/hyprland.nix
@@ -1,10 +1,6 @@
 { config, pkgs, ... }:
 
 {
-    programs.hyprland = {
-        enable = true;
-    };
-
     environment.systemPackages = with pkgs; [
       hyprpaper
       kitty
--- hosts/simple/modules/services.nix
+++ hosts/simple/modules/services.nix
@@ -3,26 +3,26 @@
 {
   services = {
 
-    # xserver.displayManager.lightdm.enable = true;
-    # xserver.desktopManager.xfce.enable = true;
+    xserver.displayManager.lightdm.enable = true;
+    xserver.desktopManager.xfce.enable = true;
   	dbus.enable = true;
 	  picom.enable = true;
 	  openssh.enable = true;
     spice-vdagentd.enable = true;
     qemuGuest.enable = true;
 
-    displayManager.sddm = {
-      enable = true;
-      wayland.enable = true;
-    };
+    # displayManager.sddm = {
+    #   enable = true;
+    #   wayland.enable = true;
+    # };
 
 	  xserver = {
 		  enable = true;
       layout = "us";
-      # xkb = {
-      #   layout = "fr";
-      #   variant = "azerty";
-      # };
+      xkb = {
+        layout = "fr";
+        variant = "azerty";
+      };
     };
   };
 }
