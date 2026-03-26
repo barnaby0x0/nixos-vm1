2c9268b4caf7272115ea9fdcc24e918ad4ff2eaa
Victor <victor@mail.com>
2026-03-26 18:10:42 +0100

    .

--- hosts/simple/modules/hyprland.nix
+++ hosts/simple/modules/hyprland.nix
@@ -28,4 +28,18 @@
     mpc
   ];
 
+  services.mpd = {
+  enable = true;
+  musicDirectory = "/home/victor/Music";
+  extraConfig = ''
+    # must specify one or more outputs in order to play audio!
+    # (e.g. ALSA, PulseAudio, PipeWire), see next sections
+  '';
+
+  # Optional:
+  network.listenAddress = "any"; # if you want to allow non-localhost connections
+  network.startWhenNeeded = true; # systemd feature: only start MPD service upon connection to its socket
+};
+
+
 }
